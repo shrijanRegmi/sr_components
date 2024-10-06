8b148a3a2746724fc8c7854514d8fb12ee74c3d5
Shrijan Regmi <your.email@example.com>
2024-10-06 11:47:29 +0545

    feat: added border to round icon button

--- lib/src/widgets/round_icon_button.dart
+++ lib/src/widgets/round_icon_button.dart
@@ -2,22 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:sr_components/sr_components.dart';
 
 class SRRoundIconButton extends StatelessWidget {
-  final Widget icon;
-  final Widget? overlayWidget;
-  final EdgeInsetsGeometry? padding;
-  final Color? bgColor;
-  final BoxShadow? shadow;
-  final Function()? onPressed;
-
   const SRRoundIconButton({
     Key? key,
     required this.icon,
     this.overlayWidget,
     this.bgColor,
+    this.border,
     this.padding,
     this.shadow,
     this.onPressed,
   }) : super(key: key);
+
+  final Widget icon;
+  final Widget? overlayWidget;
+  final EdgeInsetsGeometry? padding;
+  final Color? bgColor;
+  final BoxShadow? shadow;
+  final BoxBorder? border;
+  final Function()? onPressed;
 
   @override
   Widget build(BuildContext context) {
@@ -33,6 +35,7 @@ class SRRoundIconButton extends StatelessWidget {
               decoration: BoxDecoration(
                 shape: BoxShape.circle,
                 color: bgColor ?? SRColors.extraLightGrey.withOpacity(0.2),
+                border: border,
                 boxShadow: [
                   shadow ??
                       const BoxShadow(
